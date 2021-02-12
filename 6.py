6587075dc03dea81b309b780db00d53e78881f4f
Apollo Chiu <eldorado.tw@gmail.com>
2021-02-12 16:02:21 +0800

    My while new method

---
+++ 6.py
@@ -0,0 +1,53 @@
+x = float
+print(x)
+
+x=5.0
+print(x)
+
+y = bool
+print(y)
+
+y = '88'
+
+y = int(y)
+print(y)
+
+y = 99
+print(y)
+
+
+y =False
+print(y)
+
+
+y = 'Allo'
+print(y)
+
+
+z = "Allen"
+print(z)
+
+z = 55
+print(z)
+
+
+while 1:
+	print('3Q')
+	print()
+	break
+
+i = 3
+
+while i:
+	print('5Q')
+	
+	i -= 1
+
+a = 100
+b = str(a)
+b1 = 'abc'
+c = b1 + b
+print(c)
+
+
+
